7cc810c96bea94ed8144ba98445543f2edcd5b64
lixxce5017 <lixxce5017@gmail.com>
2025-07-11 16:40:52 +0900

    Initial commit

---
+++ src/main/java/com/nighttrip/core/NigthtripApplication.java
@@ -0,0 +1,13 @@
+package com.nighttrip.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NigthtripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NigthtripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nighttrip/core/NigthtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nighttrip.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NigthtripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
