af26cdeed21bb08ec6895cb695a94267ac9a3d18
user <dddsa1313@gmail.com>
2025-07-17 16:00:52 +0900

    feat: Spring Security 기반 CustomOAuth2UserService 및 사용자 정보 관련 클래스 추가

---
+++ src/main/java/com/nighttrip/core/oauth/userinfo/NaverUserInfo.java
@@ -0,0 +1,34 @@
+package com.nighttrip.core.oauth.userinfo;
+
+import com.nighttrip.core.global.enums.OauthProvider;
+
+import java.util.Map;
+
+public class NaverUserInfo implements OAuthUserInfo {
+
+    private final Map<String, Object> attributes;
+
+    public NaverUserInfo(Map<String, Object> attributes) {
+        this.attributes = (Map<String, Object>) attributes.get("response");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getNickname() {
+        return (String) attributes.get("nickname");
+    }
+
+    @Override
+    public String getSocialId() {
+        return (String) attributes.get("id");
+    }
+
+    @Override
+    public OauthProvider getProvider() {
+        return OauthProvider.NAVER;
+    }
+}
