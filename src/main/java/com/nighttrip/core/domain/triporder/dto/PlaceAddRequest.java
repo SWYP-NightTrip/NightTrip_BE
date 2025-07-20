875fd967655e3915b0f358a391f843c60c5962c8
usingjun <usingjun99@gmail.com>
2025-07-21 01:57:00 +0900

    refactor : 여행 장소 추가 api 변경

--- src/main/java/com/nighttrip/core/domain/triporder/dto/PlaceAddRequest.java
+++ src/main/java/com/nighttrip/core/domain/triporder/dto/PlaceAddRequest.java
@@ -3,8 +3,6 @@ package com.nighttrip.core.domain.triporder.dto;
 public record PlaceAddRequest(
         String placeName,
         String placeAddress,
-        String placeDetailAddress,
-        String tripPlanName,
-        Integer tripDayOrder
+        String placeDetailAddress
 ) {
 }
