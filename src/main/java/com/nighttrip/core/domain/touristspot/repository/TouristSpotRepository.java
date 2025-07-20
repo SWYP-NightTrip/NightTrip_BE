0fe1d8c60ac3e2458405202e372d51ab2468f217
usingjun <usingjun99@gmail.com>
2025-07-21 01:58:18 +0900

    feat : 여행지 순서 변경

--- src/main/java/com/nighttrip/core/domain/touristspot/repository/TouristSpotRepository.java
+++ src/main/java/com/nighttrip/core/domain/touristspot/repository/TouristSpotRepository.java
@@ -5,16 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 public interface TouristSpotRepository extends JpaRepository<TouristSpot, Long> {
 
     @Query("""
-                select max(t.order)
+                select max(t.orderIndex)
                 from TripOrder t
                 where t.tripDay.tripPlan.id=:tripPlanId
-                and t.tripDay.order=:tripDayOrder
+                and t.tripDay.dayOrder=:tripDayOrder
             """)
-    Optional<Integer> findLastOrder(@Param("tripPlanId") Long tripPlanId, @Param("tripDayOrder") Integer tripDayOrder);
+    Optional<BigDecimal> findLastOrder(@Param("tripPlanId") Long tripPlanId, @Param("tripDayOrder") Integer tripDayOrder);
 
 }
