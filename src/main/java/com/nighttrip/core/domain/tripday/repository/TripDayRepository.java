1c4596aefea1ac889f49722df30fbea8a6ec1a55
usingjun <usingjun99@gmail.com>
2025-07-21 01:08:18 +0900

    feat : 여행지 순서 변경 api

--- src/main/java/com/nighttrip/core/domain/tripday/repository/TripDayRepository.java
+++ src/main/java/com/nighttrip/core/domain/tripday/repository/TripDayRepository.java
@@ -3,12 +3,11 @@ package com.nighttrip.core.domain.tripday.repository;
 import com.nighttrip.core.domain.tripday.entity.TripDay;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface TripDayRepository extends JpaRepository<TripDay, Long> {
 
-    @Query("select td from TripDay td where td.tripPlan.title=:tripPlanName and td.order=:tripDayOrder")
-    Optional<TripDay> findByTripPlanNameAndDayOrder(@Param("tripPlanName") String tripPlanName, @Param("tripDayOrder") Integer tripDayOrder);
+    @Query("select td from TripDay td where td.tripPlan.id=:tripPlanId and td.dayOrder=:tripDayId")
+    Optional<TripDay> findByTripPlanIdAndTripDayId(Long tripPlanId, Integer tripDayId);
 }
