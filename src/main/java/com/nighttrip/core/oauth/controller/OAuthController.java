3e4882b87c6f90d7909097b71e52afe971b184e1
lixxce5017 <lixxce5017@gmail.com>
2025-07-30 19:24:52 +0900

    refator: 쿠키 저장안되는,시큐리티 컨피그 변경

--- src/main/java/com/nighttrip/core/oauth/controller/OAuthController.java
+++ src/main/java/com/nighttrip/core/oauth/controller/OAuthController.java
@@ -23,6 +23,7 @@ public class OAuthController {
         log.info(">>>> [API-CHECK-START] /api/v1/oauth/status 컨트롤러가 호출되었습니다.");
 
         LoginStatusResponse response = oAuthService.getLoginStatus();
+        log.info(">>>> 로그인 상태 응답: {}", response);
         log.info(">>>> [API-CHECK-END] /api/v1/oauth/status 컨트롤러가 응답을 반환합니다.");
         return ResponseEntity.ok(ApiResponse.success(response));
     }
