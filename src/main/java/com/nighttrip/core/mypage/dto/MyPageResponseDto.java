d0caf26c51de8f1688b5cc6d255d1f100c170362
user <dddsa1313@gmail.com>
2025-07-31 22:53:11 +0900

    feat: 마이페이지 응답을 위한 DTO 추가

---
+++ src/main/java/com/nighttrip/core/mypage/dto/MyPageResponseDto.java
@@ -0,0 +1,17 @@
+package com.nighttrip.core.mypage.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class MyPageResponseDto {
+    private String userName;
+    private String userAvatarUrl;
+    private int level;
+    private long bookmarkedSpotsCount;
+    private long likedSpotsCount;
+    private List<RecentPlanDto> recentPlans;
+}
