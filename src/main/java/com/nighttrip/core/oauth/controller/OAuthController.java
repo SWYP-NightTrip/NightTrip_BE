030da131011dc1a6556c0bffebc52a0fb314b6a5
lixxce5017 <lixxce5017@gmail.com>
2025-08-02 03:01:54 +0900

    chore: 쿠키 받기위한 yaml 파일 수정

--- src/main/java/com/nighttrip/core/oauth/controller/OAuthController.java
+++ src/main/java/com/nighttrip/core/oauth/controller/OAuthController.java
@@ -10,11 +10,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/oauth")
 public class OAuthController {
 
     private final OAuthService oAuthService;
