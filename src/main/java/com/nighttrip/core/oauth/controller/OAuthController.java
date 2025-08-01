c35f78fd95c851566b0f4f63dc0dd81861ba0da3
lixxce5017 <lixxce5017@gmail.com>
2025-08-02 02:40:38 +0900

    chore: status 메소드 log 추가

--- src/main/java/com/nighttrip/core/oauth/controller/OAuthController.java
+++ src/main/java/com/nighttrip/core/oauth/controller/OAuthController.java
@@ -1,5 +1,6 @@
 package com.nighttrip.core.oauth.controller;
 
+import com.nighttrip.core.domain.user.dto.UserInfoResponse;
 import com.nighttrip.core.global.dto.ApiResponse;
 import com.nighttrip.core.oauth.dto.LoginStatusResponse;
 import com.nighttrip.core.oauth.service.OAuthService;
@@ -22,7 +23,29 @@ public class OAuthController {
 
     @GetMapping("/status")
     public ResponseEntity<ApiResponse<LoginStatusResponse>> checkLoginStatus() {
+        // [1] oAuthService.getLoginStatus() 메서드 호출 전 로그
+        log.info(">>>> [checkLoginStatus] 로그인 상태 확인 요청이 들어왔습니다.");
+
         LoginStatusResponse loginStatus = oAuthService.getLoginStatus();
+
+        // [2] LoginStatusResponse 객체 전체 값 로깅
+        log.info(">>>> [checkLoginStatus] oAuthService.getLoginStatus() 호출 결과: {}", loginStatus);
+
+        // [3] LoginStatusResponse 객체 내부 값 상세 로깅
+        log.info(">>>> [checkLoginStatus] isLoggedIn 값: {}", loginStatus.isLoggedIn());
+
+        if (loginStatus.isLoggedIn()) {
+            UserInfoResponse userInfo = loginStatus.getUserInfo();
+
+            log.info(">>>> [checkLoginStatus] userInfo 객체가 존재합니다. 상세 정보 로깅을 시작합니다.");
+            log.info(">>>> [checkLoginStatus] User ID: {}", userInfo.getUserId());
+            log.info(">>>> [checkLoginStatus] Email: {}", userInfo.getEmail());
+            log.info(">>>> [checkLoginStatus] Nickname: {}", userInfo.getNickname());
+            // 필요한 경우 다른 UserInfoResponse 필드도 추가로 로깅 가능
+        } else {
+            log.info(">>>> [checkLoginStatus] 현재 로그인 상태가 아닙니다.");
+        }
+
         return ResponseEntity.ok(ApiResponse.success(loginStatus));
     }
 
