943f3d5aed498bcb65443db7eea8425f82d41da6
lixxce5017 <lixxce5017@gmail.com>
2025-08-01 23:27:36 +0900

    chore: 쿠키 저장을 위한 시큐리티 세임사이트 필터 추가

--- src/main/java/com/nighttrip/core/global/config/SameSiteCookieFilter.java
+++ src/main/java/com/nighttrip/core/global/config/SameSiteCookieFilter.java
@@ -2,9 +2,11 @@ package com.nighttrip.core.global.config;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.util.Collection;
-
+@Slf4j
 public class SameSiteCookieFilter implements Filter {
 
     @Override
@@ -18,6 +20,7 @@ public class SameSiteCookieFilter implements Filter {
             boolean firstHeader = true;
             for (String header : headers) {
                 if (header.startsWith("SESSION")) {
+                    log.info("[SameSiteCookieFilter] Set-Cookie 헤더 가공 시도");
                     String updatedHeader = header.replace("SameSite=Lax", "SameSite=None; Secure");
                     if (firstHeader) {
                         httpResp.setHeader("Set-Cookie", updatedHeader);
