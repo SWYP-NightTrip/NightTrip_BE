f1e6bf9eb42432b745a38e63f21e9ab4911e29ab
lixxce5017 <lixxce5017@gmail.com>
2025-08-06 04:26:18 +0900

    feat:자동완성 검색 테스트 파일 추가

--- src/main/java/com/nighttrip/core/global/config/SecurityConfig.java
+++ src/main/java/com/nighttrip/core/global/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/main/**").permitAll()
                         .requestMatchers("/api/v1/search/**", "/api/v1/search/recommend", "/api/v1/search/popular", "/api/v1/test/login").permitAll()
                         .requestMatchers("/oauth2/**").permitAll()
-                        .requestMatchers("/favicon.ico").permitAll()
+                        .requestMatchers("/favicon.ico", "/static/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/touristspot/*").permitAll()
                         .requestMatchers(HttpMethod.GET, "/health").permitAll()
                         .anyRequest().authenticated()
