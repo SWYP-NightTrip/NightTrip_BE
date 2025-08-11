99c88e35d1aa4948146d2d5627a0b21bc32d9bab
user <dddsa1313@gmail.com>
2025-08-12 02:12:31 +0900

    feat: 도시 검색 기능 구현

--- src/main/java/com/nighttrip/core/global/enums/ErrorCode.java
+++ src/main/java/com/nighttrip/core/global/enums/ErrorCode.java
@@ -21,6 +21,8 @@ public enum ErrorCode {
 
     GEOCODE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "지오코딩 중 실패했습니다."),
 
+	EMPTY_SEARCH_KEYWORD(HttpStatus.BAD_REQUEST, "검색어는 비어 있을 수 없습니다."),
+
 	INVALID_OAUTH_CODE_MISSING(HttpStatus.BAD_REQUEST, "인가 코드가 누락되었습니다."),
 	INVALID_OAUTH_CODE_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 인가 코드입니다."),
 	UNSUPPORTED_SOCIAL_PROVIDER(HttpStatus.BAD_REQUEST, "지원하지 않는 소셜 로그인입니다."),
