15105ebbf1c72e75e2540cb0c8a038142f8cab30
usingjun <usingjun99@gmail.com>
2025-08-28 15:09:27 +0900

    feat: jpa config 추가

---
+++ src/main/java/com/nighttrip/core/global/config/MainDataSourceConfig.java
@@ -0,0 +1,65 @@
+package com.nighttrip.core.global.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.*;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+public class MainDataSourceConfig {
+
+    @Primary
+    @Bean
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties mainDsProps() {
+        return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean(name = "dataSource")
+    public DataSource dataSource(@Qualifier("mainDsProps") DataSourceProperties p) {
+        return p.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Primary
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            @Qualifier("dataSource") DataSource ds,
+            Environment env
+    ) {
+        var emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(ds);
+        emf.setPackagesToScan("com.nighttrip.core.domain"); // 메인 엔티티 패키지
+        emf.setPersistenceUnitName("main");
+
+        var vendor = new HibernateJpaVendorAdapter();
+        emf.setJpaVendorAdapter(vendor);
+
+        var props = new HashMap<String, Object>();
+        // application.yml의 설정을 따르도록 기본값만 세팅
+        props.put("hibernate.hbm2ddl.auto", env.getProperty("spring.jpa.hibernate.ddl-auto", "update"));
+        // Dialect는 자동 감지되지만, 명시하고 싶으면 아래 주석 해제
+        // props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        emf.setJpaPropertyMap(props);
+        return emf;
+    }
+
+    @Primary
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("entityManagerFactory") EntityManagerFactory emf
+    ) {
+        return new JpaTransactionManager(emf);
+    }
+}
